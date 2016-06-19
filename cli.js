49321f0003f178e8e94c2a0079d21f0282ab4ee6
Chris Alfano <chris@jarv.us>
2016-06-18 22:06:22 -0700

    Add scaffold for cli script

---
+++ cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var flatiron = require('flatiron'),
+    path = require('path'),
+    app = flatiron.app;
+
+// export app instance
+module.exports = app;
+
+// load environment into config
+app.config.env();
+
+// configure flatiron cli plugin
+app.use(flatiron.plugins.cli, {
+    source: path.join(__dirname, 'commands'),
+    usage: 'Empty Flatiron Application, please fill out commands',
+    argv: {
+        debug: {
+            alias: 'd',
+            description: 'Show debug output',
+            boolean: true
+        }
+    }
+});
+
+// start app
+app.start({
+    log: {
+        level: app.argv.debug ? 'debug' : 'info'
+    }
+});
