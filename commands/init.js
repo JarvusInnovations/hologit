a173127f09e7e8b24e617275a3d87700fe907c59
Chris Alfano <chris@jarv.us>
2016-06-19 22:08:22 -0700

    WIP: Start building init command

--- commands/init.js
+++ commands/init.js
@@ -1,26 +1,75 @@
 var log = require('../lib/log'),
-    Git = require('../lib/git');
+    Git = require('../lib/git'),
+    async = require('async');
 
 
 /**
- * Initialize a harmony repository
- * - If local subrepo doesn't exist, move current repo there and initialize fresh repository with a commit of local merged into master with a tagged commit
+ * Initialize a harmony branch
+ * - [X] Check if branch exists already (die for now, merge on top of later)
+ * - Load sources and mounts from topBranch
+ * - Loop sources and generate commit for each
+ * - Merge new commit onto virtualBranch
  */
 module.exports = function(topBranch, virtualBranch, callback) {
-    log.info('emergence-harmony-init');
+
+    if (!topBranch) {
+        log.error('topBranch required');
+        process.exit(1);
+    }
+
+    if (!virtualBranch) {
+        log.error('virtualBranch required');
+        process.exit(1);
+    }
+
+    log.info('emergence-harmony-init', { topBranch: topBranch, virtualBranch: virtualBranch });
 
     var git = new Git();
-    log.info('initialied git wrapper...', git);
 
-    Git.getGitDirFromEnvironment(function(error, gitDir) {
-        log.info('got git dir from env', {error: error, gitDir: gitDir});
+    async.auto({
+        getTopBranch: function(callback) {
+            git.exec('show-ref', { s: true }, 'refs/heads/' + topBranch, function(error, sha) {
+                callback(null, sha || null);
+            });
+        },
+        getVirtualBranch: function(callback) {
+            git.exec('show-ref', { s: true }, 'refs/heads/' + virtualBranch, function(error, sha) {
+                callback(null, sha || null);
+            });
+        },
+        getGitDir: function(callback) {
+            git.exec('rev-parse', { 'git-dir': true }, callback);
+        },
+        getSomethingElse: [
+            'getTopBranch',
+            'getVirtualBranch',
+            'getGitDir',
+            function(results, callback) {
+                var topBranchSha = results.getTopBranch,
+                    virtualBranchSha = results.getVirtualBranch;
+
+                if (!topBranchSha) {
+                    return callback('branch ' + topBranch + ' not found');
+                }
+
+                if (virtualBranchSha) {
+                    // TODO: allow and apply merge instead
+                    return callback('branch ' + virtualBranch + ' already exists');
+                }
+
+                debugger;
+                callback();
+            }
+        ]
+    }, function(error, results) {
+        if (error) {
+            log.error(error);
+            process.exit(1);
+        }
+
+        log.info('results', results);
+        debugger;
     });
-
-    Git.getWorkTreeFromEnvironment(function(error, workTree) {
-        log.info('got work tree from env', {error: error, workTree: workTree});
-    });
-
-    callback();
 };
 
 module.exports.usage = [
