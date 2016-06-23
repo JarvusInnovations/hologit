7fbf35131cbbfd13b3146859308ac599c94b7b11
Chris Alfano <chris@jarv.us>
2016-06-22 21:22:53 -0700

    Rename pull command to test

---
+++ commands/test.js
@@ -0,0 +1,13 @@
+module.exports = (cli) => cli
+    .command('test <holo-branch>')
+    .description('update a given holo branch from its sources')
+    .action(test);
+
+
+var logger = require('../lib/logger');
+
+
+function test(cmd, options) {
+    logger.info('info from test!');
+    logger.debug('debug from test!');
+}
