99197fea79447554fc2ac1855b95ebb1566b335d
Chris Alfano <chris@jarv.us>
2018-06-26 00:58:36 -0400

    Fix error text

--- commands/project.js
+++ commands/project.js
@@ -19,7 +19,7 @@ exports.handler = async argv => {
         console.log(hash);
         process.exit(0);
     } catch (err) {
-        console.error('Failed to test:', err);
+        console.error('Failed to project:', err);
         process.exit(1);
     }
 };
