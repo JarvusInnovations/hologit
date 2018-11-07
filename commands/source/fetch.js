84eebb2cb535de023c6a475e53336090400db054
Chris Alfano <chris@jarv.us>
2018-11-07 11:32:22 -0500

    refactor: use new source.fetch method for fetch command

--- commands/source/fetch.js
+++ commands/source/fetch.js
@@ -20,34 +20,9 @@ exports.handler = async function fetch ({ name }) {
 
     // load .holo info
     const repo = await hololib.getRepo();
-    const source = await repo.getSource(name);
+    const source = await repo.getSource(name); // TODO: this does a fetch too...
 
 
-    // examine source
-    const { url, ref } = source.config.holosource;
-    const localRef = `refs/sources/${name}/${ref.substr(5)}`;
-
-
-    // fetch current head first
-    logger.info(`fetching head ${ref} ${source.head}`);
-    await repo.git.fetch({ depth: 1 }, url, `+${source.head}:${localRef}`);
-
-
-    // examine remote repo/branch
-    logger.info(`listing ${url}#${ref}`);
-    const lsRemoteOutput = await repo.git.lsRemote(url, ref);
-    const match = lsRemoteOutput.match(/^([0-9a-f]{40})\t(\S+)$/m);
-
-    if (!match) {
-        throw new Error(`could not find remote ref for ${ref}`);
-    }
-
-    const hash = match[1];
-
-
-    // fetch remote head
-    if (hash != source.head) {
-        logger.info(`fetching remote ${ref} ${hash}`);
-        await repo.git.fetch({ depth: 1 }, url, `+${hash}:${localRef}`);
-    }
+    // execute fetch
+    await source.fetch();
 };
