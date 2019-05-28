20d974660af32e868bd343f2e52c8708167b3566
Chris Alfano <chris@jarv.us>
2019-05-28 01:31:37 -0700

    refactor: move lens-tree and build-pages to external scripts

--- lenses/sencha-pages/plan.sh
+++ lenses/sencha-pages/plan.sh
@@ -22,55 +22,9 @@ do_install() {
   build_line "Generating lens script"
 
   pushd "${pkg_prefix}" > /dev/null
-  cat > "bin/lens-tree" <<- EOM
-#!$(pkg_path_for bash)/bin/sh
-
-INPUT_TREE="\${1?<input> required}"
-
-# redirect all output to stderr
-{
-  # export git tree to disk
-  git holo lens export-tree "\${INPUT_TREE}"
-
-  # execute compilation
-  pushd "\${GIT_WORK_TREE}" > /dev/null
-
-  sencha \
-    --sdk-path='./ext' \
-    compile \
-      --classpath='./ext/packages/core/src' \
-      --classpath='./ext/packages/core/overrides' \
-      --classpath='./pages/src' \
-      union --recursive --tag='class' \
-      and save core \
-      and concat \
-        --input-js-version='ANY' \
-        --js-version='ANY' \
-        --strip-comments \
-        --output-file='./build/common.js' \
-      and union --recursive --include-uses=no --tag='core' \
-      and require --source-name='Ext.event.publisher.Dom' --requires='Ext.GlobalEvents' \
-      and require --source-name='Ext.event.Event' --requires='Ext.dom.Fly' \
-      and include --recursive --include-uses=no --class='Site.Common' \
-      and exclude --set='core' \
-      and concat \
-        --input-js-version='ANY' \
-        --js-version='ANY' \
-        --strip-comments \
-        --append \
-        --output-file='./build/common.js'
-
-  popd > /dev/null
-
-  # add output to git index
-  git add build
-} 1>&2
-
-# output tree hash
-git write-tree --prefix=build
-
-EOM
-  chmod +x "bin/lens-tree"
+  cp "${PLAN_CONTEXT}/"{build-pages,lens-tree} "bin/"
+  fix_interpreter "bin/*" core/bash bin/bash
+  chmod +x "bin/"*
   popd > /dev/null
 }
 
