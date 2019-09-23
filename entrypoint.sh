1ad733cd4feb92572c9b5147005bd3f9da27660e
Chris Alfano <chris@jarv.us>
2019-09-23 06:08:10 +0000

    feat: dump /home and /github trees

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,15 @@ hab pkg exec core/tree tree -x -L 5 -F /
 
 echo
 echo
+echo "/github:"
+hab pkg exec core/tree tree -x -F /github
+
+echo
+echo
+echo "/home:"
+hab pkg exec core/tree tree -x -F /home
+
+echo
+echo
 echo "Env:"
 hab pkg exec core/coreutils printenv
