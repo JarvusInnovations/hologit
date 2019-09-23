71a7a9aa77873cc7e6444a93c99e8008f0760fa3
Chris Alfano <chris@jarv.us>
2019-09-23 06:19:48 +0000

    feat: run projection

--- github-actions/projector/entrypoint.sh
+++ github-actions/projector/entrypoint.sh
@@ -1,26 +1,7 @@
 #!/bin/bash -e
 
-echo "Holobranch: ${1}"
-echo "All args: $*"
+git fetch origin "refs/heads/${INPUT_COMMIT-TO}"
 
-echo ::set-output "name=last-projection::abcdef1234567890"
+PROJECTION_OUTPUT=$(git holo project "$@")
 
-echo
-echo
-echo "Filesystem:"
-hab pkg exec core/tree tree -x -L 5 -F /
-
-echo
-echo
-echo "/github:"
-hab pkg exec core/tree tree -x -F /github
-
-echo
-echo
-echo "/home:"
-hab pkg exec core/tree tree -x -F /home
-
-echo
-echo
-echo "Env:"
-hab pkg exec core/coreutils printenv
+echo ::set-output "name=last-projection::${PROJECTION_OUTPUT}"
