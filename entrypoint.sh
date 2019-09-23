53a2d3fa158f8f1050d86051911f9929c74105c6
Chris Alfano <chris@jarv.us>
2019-09-23 03:24:00 +0000

    chore: add blank links to debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ echo "Projections: ${1}"
 
 echo ::set-output "name=last-projection::abcdef1234567890"
 
+echo
+echo
 echo "Filesystem:"
 hab pkg exec core/tree tree -x -L 5 -F /
 
+echo
+echo
 echo "Env:"
 hab pkg exec core/coreutils printenv
