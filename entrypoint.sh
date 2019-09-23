eacfeedc4de7bcd6d6baaa32301f119e52254a45
Chris Alfano <chris@jarv.us>
2019-09-23 03:22:11 +0000

    chore: add a bunch of debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
-echo "Projections: $1"
+echo "Projections: ${1}"
 
 echo ::set-output "name=last-projection::abcdef1234567890"
+
+echo "Filesystem:"
+hab pkg exec core/tree tree -x -L 5 -F /
+
+echo "Env:"
+hab pkg exec core/coreutils printenv
