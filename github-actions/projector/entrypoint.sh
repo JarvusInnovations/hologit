e49de49b95e7d11179358c24dcf318aa36d2b514
Chris Alfano <chris@jarv.us>
2019-09-23 11:13:53 -0400

    fix: read env input with hyphen

--- github-actions/projector/entrypoint.sh
+++ github-actions/projector/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -e
 
-git update-ref "refs/heads/${INPUT_COMMIT-TO}" "refs/remotes/origin/${INPUT_COMMIT-TO}"
+# port hyphenated env to underscored, unless already set
+INPUT_COMMIT_TO_HYPHENATED='INPUT_COMMIT-TO'
+if [ -z "${INPUT_COMMIT_TO}" ] && [ -n "${!INPUT_COMMIT_TO_HYPHENATED}" ]; then
+    export INPUT_COMMIT_TO="${!INPUT_COMMIT_TO_HYPHENATED}"
+fi
 
+# grab local copy of commit-to ref
+if [ -n "${INPUT_COMMIT_TO}" ]; then
+    git update-ref "refs/heads/${INPUT_COMMIT_TO}" "refs/remotes/origin/${INPUT_COMMIT_TO}"
+fi
+
+# run projection and return output
 PROJECTION_OUTPUT=$(git holo project "$@")
-
 echo ::set-output "name=last-projection::${PROJECTION_OUTPUT}"
