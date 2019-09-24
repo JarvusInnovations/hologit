e5d4c5597088369a1654ee40d4f4bb6ede5c4eea
Chris Alfano <chris@jarv.us>
2019-09-24 00:59:29 -0400

    feat: wrap all steps in their own error handling

--- github-actions/projector/index.js
+++ github-actions/projector/index.js
@@ -2,78 +2,137 @@ const core = require('@actions/core');
 const { exec } = require('@actions/exec');
 const io = require('@actions/io');
 
+
+// gather input
 const { GITHUB_ACTOR, GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_REF, GITHUB_SHA } = process.env;
 
-const holobranch = core.getInput('holobranch', { required: false });
+const holobranch = core.getInput('holobranch', { required: true });
 const lens = core.getInput('lens');
 const commitTo = core.getInput('commit-to', { required: false });
 const commitToRef = commitTo == 'HEAD' || commitTo.startsWith('refs/')
     ? commitTo
-    : `refs/heads/${commitTo}`
+    : `refs/heads/${commitTo}`;
 
-core.info('Inputs: '+JSON.stringify({ holobranch, lens, commitTo }, null, 4))
 
+// run with error wrapper
 try {
     run();
 } catch(err) {
     core.setFailed(err.message);
 }
 
-
 async function run() {
-    core.startGroup(`Initializing git repository: ${GITHUB_REPOSITORY}`);
-    await exec('git init --bare');
-    await exec('git remote add', [
-        'origin',
-        `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`
-    ]);
-    core.endGroup();
+    const repoInitialized = await exec('git rev-parse --git-dir', [], { ignoreReturnCode: true, silent: true }) === 0;
 
-    core.startGroup(`Fetching: ${GITHUB_REF}`);
-    await exec ('git fetch', [
-        '--tags',
-        '--no-recurse-submodules',
-        '--depth=1',
-        'origin',
-        `${GITHUB_REF}:${GITHUB_REF}`
-    ]);
-    core.endGroup();
+    if (!repoInitialized) {
+        core.startGroup(`Initializing git repository: ${GITHUB_REPOSITORY}`);
+        try {
+            await exec('git init --bare');
+            await exec('git remote add', [
+                'origin',
+                `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`
+            ]);
+        } catch (err) {
+            core.setFailed(`Failed to initialize git repository: ${err.message}`);
+            return;
+        } finally {
+            core.endGroup();
+        }
+    }
 
-    core.startGroup(`Fetching: ${commitToRef}`);
-    await exec ('git fetch', [
-        '--no-recurse-submodules',
-        '--depth=1',
-        'origin',
-        `${commitToRef}:${commitToRef}`
-    ]);
-    core.endGroup();
 
-    if (!await io.which('hab')) {
-        core.startGroup('Installing Chef Habitat');
-        await exec('wget https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh');
-        await exec('sudo bash install.sh');
-        await io.rmRF('install.sh');
+    try {
+        core.startGroup(`Fetching: ${GITHUB_REF}`);
+        await exec ('git fetch', [
+            '--tags',
+            '--no-recurse-submodules',
+            '--depth=1',
+            'origin',
+            `${GITHUB_REF}:${GITHUB_REF}`
+        ]);
+    } catch (err) {
+        core.setFailed(`Failed to fetch GITHUB_REF: ${err.message}`);
+        return;
+    } finally {
         core.endGroup();
     }
 
-    core.startGroup('Initializing Habitat Studio');
-    await exec('hab studio new');
-    core.endGroup();
 
-    core.startGroup('Installing Jarvus Hologit into Habitat Studio');
-    await exec('hab studio run hab pkg install jarvus/hologit');
-    core.endGroup();
+    try {
+        core.startGroup(`Fetching: ${commitToRef}`);
+        await exec ('git fetch', [
+            '--no-recurse-submodules',
+            '--depth=1',
+            'origin',
+            `${commitToRef}:${commitToRef}`
+        ]);
+    } catch (err) {
+        core.setFailed(`Failed to fetch commit-to ref: ${err.message}`);
+        return;
+    } finally {
+        core.endGroup();
+    }
 
-    core.startGroup(`Projecting holobranch: ${holobranch}`);
-    await exec('hab studio run', [
-        'hab pkg exec jarvus/hologit',
-        'git holo project emergence-site',
-        `--ref=${GITHUB_SHA}`,
-        `--commit-to=${commitToRef}`
-    ]);
-    core.endGroup();
+    if (!await io.which('hab')) {
+        try {
+            core.startGroup('Installing Chef Habitat');
+            await exec('wget https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh');
+            await exec('sudo bash install.sh');
+            await io.rmRF('install.sh');
+        } catch (err) {
+            core.setFailed(`Failed to install Chef Habitat: ${err.message}`);
+            return;
+        } finally {
+            core.endGroup();
+        }
+    }
 
-    core.startGroup(`Pushing: ${commitToRef}`);
-    await exec('git push', ['origin', commitToRef]);
-    core.endGroup();
+
+    try {
+        core.startGroup('Initializing Habitat Studio');
+        await exec('hab studio new');
+    } catch (err) {
+        core.setFailed(`Failed to initialize Habitat Studio: ${err.message}`);
+        return;
+    } finally {
+        core.endGroup();
+    }
+
+
+    try {
+        core.startGroup('Installing Jarvus Hologit into Habitat Studio');
+        await exec('hab studio run hab pkg install jarvus/hologit');
+    } catch (err) {
+        core.setFailed(`Failed to install Jarvus Hologit: ${err.message}`);
+        return;
+    } finally {
+        core.endGroup();
+    }
+
+
+    try {
+        core.startGroup(`Projecting holobranch: ${holobranch}`);
+        await exec('hab studio run', [
+            'hab pkg exec jarvus/hologit',
+            'git holo project emergence-site',
+            `--ref=${GITHUB_SHA}`,
+            `--commit-to=${commitToRef}`
+        ]);
+    } catch (err) {
+        core.setFailed(`Failed to project holobranch: ${err.message}`);
+        return;
+    } finally {
+        core.endGroup();
+    }
+
+
+    try {
+        core.startGroup(`Pushing: ${commitToRef}`);
+        await exec('git push', ['origin', commitToRef]);
+    } catch (err) {
+        core.setFailed(`Failed to push commit-to ref: ${err.message}`);
+        return;
+    } finally {
+        core.endGroup();
+    }
 }
