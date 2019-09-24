2e3ad3dc559a82f0b34f73d809931452acf10f4c
Chris Alfano <chris@jarv.us>
2019-09-23 23:22:16 -0400

    feat: scaffold javascript action

---
+++ github-actions/projector/index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const io = require('@actions/io');
+
+run();
+
+async function run() {
+    core.startGroup('Installing Chef Habitat');
+    await exec('wget https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh');
+    await exec('bash install.sh');
+    await io.rmRF('install.sh');
+    core.endGroup();
+}
