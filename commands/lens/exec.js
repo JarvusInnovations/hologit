cafd549c053215b422ea8daa3d6a6a11cf80a244
Chris Alfano <chris@jarv.us>
2019-12-24 12:07:50 -0500

    refactor: purify lens-exec command to take no options, always exec

--- commands/lens/exec.js
+++ commands/lens/exec.js
@@ -1,26 +1,10 @@
 exports.command = 'exec <spec-hash>';
 exports.desc = 'Execute lens spec and output result hash';
-exports.builder = {
-    refresh: {
-        describe: 'True to execute lens even if existing result is available',
-        type: 'boolean',
-        default: false
-    },
-    save: {
-        describe: 'False to disable updating spec ref with result',
-        type: 'boolean',
-        default: true
-    }
-};
 
-exports.handler = async function exportTree ({
-    specHash,
-    refresh=false,
-    save=true
-}) {
+exports.handler = async function exportTree ({ specHash }) {
     const Lens = require('../../lib/Lens.js');
 
-    const lensedTreeHash = await Lens.executeSpec(specHash, { refresh, save });
+    const lensedTreeHash = await Lens.executeSpec(specHash, { refresh: true });
 
     if (lensedTreeHash) {
         console.log(lensedTreeHash);
